0f768b4aa456a04e5bd24785a7d21b197b339fb2
Mary West <westmarye1219@gmail.com>
2019-08-30 10:25:54 -0500

    created classes

---
+++ vehicles.py
@@ -0,0 +1,24 @@
+class Vehicle:
+
+   def __init__(self, color, brand, maximum_occupancy, year):
+    self.color = color
+    self.brand = brand
+    self.maximum_occupancy = maximum_occupancy
+    self.year = year
+    self.doors = doors
+
+class Tesla(Vehicle):
+
+    def __init__(self, range):
+      self.mile_range = mile_range
+    super().__init__("black", 24)
+
+tesla_car = Tesla("Betty")
+print(tesla_car)
+
+class BMW(Vehicle):
+    def __init__(self, leather):
+      self.leather_color = leather_color
+    super().__init__("black", "tan")
+
+print(tesla_car.color)
